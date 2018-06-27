ee3fec1e87873c5571fabb820e111d9c9900f971
Kevin W <rocwang0316@gmail.com>
2018-06-27 13:09:50 -0700

    Send a isFail mark when the authentication fails.

--- src/routers/functions/GetLoginWithPassword.js
+++ src/routers/functions/GetLoginWithPassword.js
@@ -5,12 +5,12 @@ const bcrypt = require('bcrypt');
 
 module.exports = (req, res) =>
   mongodb.findUserWithUsername(req.query.username).then(result => {
-    if (result.length === 0) res.json({});
+    if (result.length === 0) res.json({ isFail: true });
     else {
       bcrypt.compare(req.query.password, result[0].password).then(compareResult => {
         if (compareResult) {
           res.json(JWTUtil.signJWT(result[0]));
-        } else res.json({});
+        } else res.json({ isFail: true });
       });
     }
   }).catch(err => logger.error('/usernamePasswordLogin', err));
