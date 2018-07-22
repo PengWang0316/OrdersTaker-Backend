57f56a8b45d5ad33364b431b39b5301dd7e27405
Kevin W <rocwang0316@gmail.com>
2018-07-22 16:55:25 -0700

    Fixed some small bug and add more testing code.

--- __tests__/functions/GetFetchOrderAmount.js
+++ __tests__/functions/GetFetchOrderAmount.js
@@ -7,21 +7,30 @@ jest.mock('../../src/MongoDB', () => ({ fetchOrderAmount: jest.fn().mockReturnVa
 describe('GetFetchOrderAmount', () => {
   test('getFetchOrderAmount without error', async () => {
     const Logger = require('../../src/utils/Logger');
-    const req = { query: { user: { jwtMessage: 'jwtMessage' } } };
+    const MongoDB = require('../../src/MongoDB');
+    const JWTUtil = require('../../src/utils/JWTUtil');
+    const req = { query: { jwtMessage: 'jwtMessage' } };
     const res = { end: jest.fn() };
     await getFetchOrderAmount(req, res);
+    expect(MongoDB.fetchOrderAmount).toHaveBeenCalledTimes(1);
+    expect(MongoDB.fetchOrderAmount).toHaveBeenLastCalledWith('id');
     expect(res.end).toHaveBeenCalledTimes(1);
     expect(Logger.error).not.toHaveBeenCalled();
+    expect(JWTUtil.verifyJWT).toHaveBeenCalledTimes(1);
+    expect(JWTUtil.verifyJWT).toHaveBeenLastCalledWith('jwtMessage');
   });
 
   test('getFetchOrderAmount with database error', async () => {
     const Logger = require('../../src/utils/Logger');
+    const JWTUtil = require('../../src/utils/JWTUtil');
     const MongoDB = require('../../src/MongoDB');
     MongoDB.fetchOrderAmount.mockReturnValueOnce(Promise.reject());
-    const req = { query: { user: { jwtMessage: 'jwtMessage' } } };
+    const req = { query: { jwtMessage: 'jwtMessageA' } };
     const res = { end: jest.fn() };
     await getFetchOrderAmount(req, res);
     expect(res.end).not.toHaveBeenCalled();
     expect(Logger.error).toHaveBeenCalledTimes(1);
+    expect(JWTUtil.verifyJWT).toHaveBeenCalledTimes(2);
+    expect(JWTUtil.verifyJWT).toHaveBeenLastCalledWith('jwtMessageA');
   });
 });
