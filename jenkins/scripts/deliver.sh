f2de37ba111516aafb86d02dd4428c8d86ea954e
Kevin W <rocwang0316@gmail.com>
2018-08-27 13:01:10 -0700

    Using a deliver.sh file to contain the scripts.

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,1 +1,8 @@
-ssh -i /home/orderstaker.pem ubuntu@orderstaker.kevin-project.com "node -v"
+echo 'Stopping the pm2 Node instance'
+ssh -o StrictHostKeyChecking=no ubuntu@orderstaker.kevin-project.com sudo pm2 stop App
+echo 'Copying the package.json file to the remote server'
+scp -r -o StrictHostKeyChecking=no /var/jenkins_home/workspace/OrdersTakerBackEnd/package.json ubuntu@orderstaker.kevin-project.com:NodeFiles/
+echo 'Copying all source code to the remote server'
+scp -r -o StrictHostKeyChecking=no /var/jenkins_home/workspace/OrdersTakerBackEnd/src ubuntu@orderstaker.kevin-project.com:NodeFiles/
+echo 'Changing the working directory, run npm i and starting a pm2 Node instance'
+ssh -o StrictHostKeyChecking=no ubuntu@orderstaker.kevin-project.com \'bash -c \"cd NodeFiles && npm i && sudo pm2 start src/App.js\"\'
