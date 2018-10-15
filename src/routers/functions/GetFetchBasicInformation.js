75bba1e2a2326528bb92ffac916dfd8c1b8e249e
Kevin Wang <rocwang0316@gmail.com>
2018-10-15 11:22:18 -0700

    Deleted two router functions and thire tests.

--- src/routers/functions/GetFetchBasicInformation.js
+++
@@ -1,6 +0,0 @@
-const logger = require('../../utils/Logger');
-const mongodb = require('../../MongoDB');
-
-module.exports = (req, res) =>
-  mongodb.fetchBasicInformation()
-    .then(result => res.json(result)).catch(err => logger.error('/fetchBasicInformation', err));
