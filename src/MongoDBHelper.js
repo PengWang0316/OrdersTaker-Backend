a25fd21097c62314597ff294aab047b70006d57e
Kevin Wang <rocwang0316@gmail.com>
2018-10-15 11:43:03 -0700

    Organized some coding style.

--- src/MongoDBHelper.js
+++ src/MongoDBHelper.js
@@ -1,6 +1,6 @@
 const mongodb = require('mongodb');
 
-const MongoClient = mongodb.MongoClient;
+const { MongoClient } = mongodb;
 const winston = require('winston');
 
 require('dotenv').config();
@@ -11,7 +11,7 @@ const DB_URL = process.env.DB_HOST;
 // const COLLECTION_MENUS = 'Menus';
 // const COLLECTION_ORDERS = 'orders';
 
-const DB_NAME = process.env.DB_NAME;
+const { DB_NAME } = process.env;
 let dbs;
 
 /** Setting up the Winston logger.
@@ -20,21 +20,22 @@ let dbs;
 const logger = winston.createLogger({
   level: process.env.LOGGING_LEVEL,
   transports: [
-    new (winston.transports.Console)()
-  ]
+    new (winston.transports.Console)(),
+  ],
 });
 
 /** Replaces the previous transports with those in the
 new configuration wholesale.
   * When under the production mode, log to a file.
 */
-if (process.env.NODE_ENV === 'production')
-logger.configure({
-  level: 'error',
-  transports: [
-    new (winston.transports.File)({ filename: 'error.log' })
-  ]
-});
+if (process.env.NODE_ENV === 'production') {
+  logger.configure({
+    level: 'error',
+    transports: [
+      new (winston.transports.File)({ filename: 'error.log' }),
+    ],
+  });
+}
 
 // Initializing the connection pool.
 const ininitalConnects = async () => {
@@ -44,28 +45,26 @@ const ininitalConnects = async () => {
   } catch (e) {
     logger.error('Unable to connect to the mongoDB server. Error:', e);
   }
-}
+};
 
 ininitalConnects();
 
 exports.getDB = () => dbs;
 
 /* Using Promise to wrap connection and toArray */
-exports.promiseFindResult = callback => new Promise((resolve, reject) =>
-  callback(dbs).toArray((err, result) => {
+exports.promiseFindResult = callback => new Promise((resolve, reject) => callback(dbs)
+  .toArray((err, result) => {
     if (err) reject(err);
     else resolve(result);
   }));
 
-exports.promiseNextResult = callback => new Promise((resolve, reject) =>
-  callback(dbs).next((err, result) => {
+exports.promiseNextResult = callback => new Promise((resolve, reject) => callback(dbs)
+  .next((err, result) => {
     if (err) reject(err);
     else resolve(result);
   }));
 
-exports.promiseInsertResult = callback => new Promise((resolve, reject) =>
-  callback(dbs).then(result => {
-    resolve();
-  }));
+exports.promiseInsertResult = callback => new Promise((resolve, reject) => callback(dbs)
+  .then(result => resolve()));
 
-const promiseReturnResult = callback => new Promise((resolve, reject) => resolve(callback(dbs)));
+exports.promiseReturnResult = callback => new Promise((resolve, reject) => resolve(callback(dbs)));
